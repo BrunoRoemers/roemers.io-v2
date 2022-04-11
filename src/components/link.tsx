5df2d2c444a54e14be226ebff97cf208bb80e583
Bruno Roemers <bruno@roemers.io>
2022-04-12 00:57:04 +0200

    Create link component

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -7,14 +7,51 @@ export const defaultClassNames = [
   'text-blue-600',
 ];
 
-const Link = <TState,>({children, ref, className, ...props}: GatsbyLinkProps<TState>) => {
-  console.log(className);
+const Link = <TState,>({
+  children,
+  to,
+  ref, // external link only
+  className, // use defaults if not set
+  activeClassName, // internal link only
+  partiallyActive, // internal link only
+  ...props
+}: GatsbyLinkProps<TState>) => {
+  // links that start with "http://", "https://", "mailto:" or "tel:" are considered external
+  const isExternal = /^ *(http(s)?:\/\/|mailto:|tel:)/.test(to);
+
+  const myref = React.createRef();
+
+
   // use default classes unless overwritten by user
   const cn = className ? className : defaultClassNames.join(' ');
 
-  return (
-    <GatsbyLink className={cn} {...props}>{children}</GatsbyLink>
-  )
+  if (isExternal) {
+    // external link
+    return (
+      <a
+        {...props}
+        className={cn}
+        href={to}
+        ref={ref}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    // internal link
+    return (
+      <GatsbyLink
+        {...props}
+        className={cn}
+        to={to}
+        activeClassName={activeClassName}
+        partiallyActive={partiallyActive}
+      >
+        {children}
+      </GatsbyLink>
+    )
+  }
+
 }
 
 export default Link;
