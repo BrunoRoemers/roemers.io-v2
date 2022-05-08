c8d7bfd6c0dd1526a4240e6081b4d54073106c8a
Bruno Roemers <bruno@roemers.io>
2022-05-09 01:46:51 +0200

    Create normalizeUrl util

---
+++ src/utils/string/normalize-url.spec.ts
@@ -0,0 +1,74 @@
+import normalizeUrl from "./normalize-url";
+import removeEnd from "./remove-end";
+
+describe('normalizeUrl', () => {
+
+  const cases: [string, string, string][] = [
+    [null, '/path/to/page', 'path/to/page'],
+    [undefined, '/path/to/page', 'path/to/page'],
+    ['', '/path/to/page', 'path/to/page'],
+
+    [null, 'http://roemers.io/path/to/page', 'path/to/page'],
+    [undefined, 'http://roemers.io/path/to/page', 'path/to/page'],
+    ['', 'http://roemers.io/path/to/page', 'path/to/page'],
+
+    [null, 'https://roemers.io/path/to/page', 'path/to/page'],
+    [undefined, 'https://roemers.io/path/to/page', 'path/to/page'],
+    ['', 'https://roemers.io/path/to/page', 'path/to/page'],
+
+    ['/', '/path/to/page', '/path/to/page'],
+    ['/', 'http://roemers.io/path/to/page', '/path/to/page'],
+    ['/', 'https://roemers.io/path/to/page', '/path/to/page'],
+
+    ['http://roemers.io', null, 'http://roemers.io'],
+    ['http://roemers.io', undefined, 'http://roemers.io'],
+    ['http://roemers.io', '', 'http://roemers.io'],
+
+    ['http://roemers.io/', null, 'http://roemers.io'],
+    ['http://roemers.io/', undefined, 'http://roemers.io'],
+    ['http://roemers.io/', '', 'http://roemers.io'],
+
+    ['http://roemers.io', '/', 'http://roemers.io'],
+    ['http://roemers.io', '/path/to/page', 'http://roemers.io/path/to/page'],
+    ['http://roemers.io/', '/path/to/page', 'http://roemers.io/path/to/page'],
+    ['http://roemers.io', '/path/to/page/', 'http://roemers.io/path/to/page'],
+    ['http://roemers.io', 'path/to/page/', 'http://roemers.io/path/to/page'],
+
+    ['https://roemers.io', null, 'https://roemers.io'],
+    ['https://roemers.io', undefined, 'https://roemers.io'],
+    ['https://roemers.io', '', 'https://roemers.io'],
+
+    ['https://roemers.io/', null, 'https://roemers.io'],
+    ['https://roemers.io/', undefined, 'https://roemers.io'],
+    ['https://roemers.io/', '', 'https://roemers.io'],
+
+    ['https://roemers.io', '/', 'https://roemers.io'],
+    ['https://roemers.io', '/path/to/page', 'https://roemers.io/path/to/page'],
+    ['https://roemers.io/', '/path/to/page', 'https://roemers.io/path/to/page'],
+    ['https://roemers.io', '/path/to/page/', 'https://roemers.io/path/to/page'],
+    ['https://roemers.io', 'path/to/page', 'https://roemers.io/path/to/page'],
+    ['https://roemers.io/', 'path/to/page', 'https://roemers.io/path/to/page'],
+
+    ['https://roemers.io/', 'http://google.com/test/123', 'https://roemers.io/test/123'],
+
+    // these inputs are not valid, but it should be handled like this
+    ['https://roemers.io/', 'mailto:bruno@roemers.io', 'https://roemers.io/mailto:bruno@roemers.io'],
+    [null, 'mailto:bruno@roemers.io', 'mailto:bruno@roemers.io'],
+    [undefined, 'mailto:bruno@roemers.io', 'mailto:bruno@roemers.io'],
+    ['', 'mailto:bruno@roemers.io', 'mailto:bruno@roemers.io'],
+
+    ['aaa', 'hello', 'aaa/hello'],
+    ['aaa', '/hello', 'aaa/hello'],
+    ['aaa/', 'hello', 'aaa/hello'],
+    ['aaa/', '/hello', 'aaa/hello'],
+    ['aaa', 'bbb/hello', 'aaa/bbb/hello'],
+    ['aaa', 'https://bbb/hello', 'aaa/hello'],
+  ];
+
+  for (const [siteRoot, path, expectedResult] of cases) {
+    it(`when siteRoot = '${siteRoot}' and path = '${path}' the return value should be '${expectedResult}'`, () => {
+      expect(normalizeUrl(siteRoot, path)).toBe(expectedResult);
+    })
+  }
+
+})
---
+++ src/utils/string/normalize-url.ts
@@ -0,0 +1,33 @@
+import isBlank from "./is-blank";
+import removeEnd from "./remove-end";
+import removeStart from "./remove-start";
+import substringAfter from "./substring-after";
+
+// add site root to path (if not yet present)
+// NOTE: the domain name in the path will be discarded (if present)
+const normalizeUrl = (siteRoot: string, path: string): string => {
+  const normalizedSiteRoot = removeEnd(siteRoot, '/');
+
+  if (isBlank(path)) {
+    return normalizedSiteRoot;
+  }
+
+  // if we detect a domain, remove it from the path
+  const pathDomain = path.match(/https?:\/\/[^\/]+/);
+  const pathWithoutDomain = pathDomain ? substringAfter(path, pathDomain[0]) : path;
+
+  // remove slash at beginning and end of path
+  const normalizedPath = removeStart(removeEnd(pathWithoutDomain, '/'), '/');
+
+  if (isBlank(normalizedPath)) {
+    return normalizedSiteRoot;
+  }
+
+  if (isBlank(normalizedSiteRoot) && siteRoot !== '/') {
+    return normalizedPath;
+  }
+
+  return normalizedSiteRoot + '/' + normalizedPath;
+}
+
+export default normalizeUrl;
